857f4f6a52326568788cb10f847d7e8536166d54
Frâncila Weidt Neiva <fran.weidt@gmail.com>
2020-07-16 23:14:34 -0300

    Adaptação - Explorando o Flutter
    Brincadeira de oráculo simples.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  void oracleAnswer() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          oracleAnswer();
+        },
+        child: Image.asset(
+          'images/ball$ballNumber.png',
+        ),
+      ),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink.shade100,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.pink.shade700,
+      ),
+      body: Ball(),
+    );
+  }
+}
